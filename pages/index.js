c1b27b9b32d0350cba1fe2aefa658fefa718d7bb
Denis Arlakov <darlakov@mail.ru>
2022-04-25 23:21:11 +0300

    main

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 
 import MainContainer from "../components/A/MainContainer";
+import styles from "./Index.module.css"
 
 const Index = () => {
   return (
     <>
       <MainContainer keywords={"main page"}>
-        <div>
+        <div className={styles.main}>
           <h1>Главная страница</h1>
         </div>
       </MainContainer>
